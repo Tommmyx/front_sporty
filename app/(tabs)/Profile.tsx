7a489aaf23ddc853bbc74f30183f8e4db0bccbc2
tomlecam <tom.lecam2001@gmail.com>
2024-11-20 09:47:30 +0100

    Profile updated

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { View, TouchableOpacity, Modal, Text, Image, ScrollView } from 'react-native';
+import { View, TouchableOpacity, Modal, Text, Image, ScrollView, Dimensions } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useIsFocused } from '@react-navigation/native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function ProfileIcon({ navigation }) {
+    // Responsive design test
+    const { height, width } = Dimensions.get('window');
+    const actualDimensions = {
+        height: height < width ? width : height,
+        width: width > height ? height : width,
+    };
+
     const [modalVisible, setModalVisible] = useState(false);
+    const [iconSelectionVisible, setIconSelectionVisible] = useState(false);
     const [selectedIcon, setSelectedIcon] = useState(require('../../assets/images/profile_icons/woman.png'));
     const [userData, setUserData] = useState(null);
     const isFocused = useIsFocused(); // Détecte si l'écran est en focus
@@ -12,7 +22,7 @@ export default function ProfileIcon({ navigation }) {
     const iconLibrary = [
         require('../../assets/images/profile_icons/woman.png'),
         require('../../assets/images/profile_icons/man.png'),
-        require('../../assets/images/profile_icons/totoro.png')
+        require('../../assets/images/profile_icons/totoro.png'),
     ];
 
     const loadData = async () => {
@@ -26,7 +36,7 @@ export default function ProfileIcon({ navigation }) {
 
     useEffect(() => {
         if (isFocused) {
-            loadData(); 
+            loadData();
         }
     }, [isFocused]);
 
@@ -35,16 +45,49 @@ export default function ProfileIcon({ navigation }) {
 
     return (
         <View style={{ position: 'absolute', top: 40, right: 20 }}>
+            {/* Image de base */}
             <TouchableOpacity onPress={openProfile}>
-                <Image source={selectedIcon} style={{ width: 50, height: 50, borderRadius: 30, backgroundColor: "white" }} />
+                <Image
+                    source={selectedIcon}
+                    style={{ width: 50, height: 50, borderRadius: 30, backgroundColor: 'white' }}
+                />
             </TouchableOpacity>
 
-            <Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={closeProfile}>
+            {/* Modal principal */}
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={closeProfile}
+            >
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                    <View style={{ width: 300, padding: 20, backgroundColor: 'white', borderRadius: 10 }}>
-                        <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginBottom: 20 }}>
-                            Profil Utilisateur
-                        </Text>
+                    <View style={{ width: width * 0.5, padding: 20, backgroundColor: 'white', borderRadius: 10 }}>
+                        {/* Bouton pour fermer le modal */}
+                        <TouchableOpacity onPress={closeProfile} style={{ alignSelf: 'flex-start' }}>
+                            <FontAwesomeIcon icon={faXmark} size={40} color="black" />
+                        </TouchableOpacity>
+
+                           {/* Bouton pour ouvrir la sélection d'icônes */}
+                        <TouchableOpacity onPress={() => setIconSelectionVisible(true)} 
+                                                    style={{
+                                                        
+                                                        alignItems: 'center',
+                                                    }}
+                            >
+                            <Image source={selectedIcon} style={{ width: 100, height: 100, borderRadius: 50, backgroundColor: '#fafafa' }}/>
+                        </TouchableOpacity>
+
+                        {userData ? (
+                            <>
+                            <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center'}}>
+                                {userData.username}
+                            </Text>
+                            </>
+                        ) : (
+                            <Text>Aucun profil trouvé</Text>
+                        )}
+
+                        {/* Affichage des données utilisateur */}
                         {userData ? (
                             <>
                                 <Text>Pseudo: {userData.username}</Text>
@@ -58,16 +101,42 @@ export default function ProfileIcon({ navigation }) {
                         ) : (
                             <Text>Aucun profil trouvé</Text>
                         )}
-                        <ScrollView contentContainerStyle={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', marginTop: 20 }}>
+
+                     
+                    </View>
+                </View>
+            </Modal>
+
+            {/* Modal pour sélectionner une icône */}
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={iconSelectionVisible}
+                onRequestClose={() => setIconSelectionVisible(false)}
+            >
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                    <View style={{ width: width * 0.5, padding: 20, backgroundColor: 'white', borderRadius: 10 }}>
+                        <TouchableOpacity
+                            onPress={() => setIconSelectionVisible(false)}
+                            style={{alignSelf: 'flex-start' }}
+                        >
+                            <FontAwesomeIcon icon={faXmark} size={40} color="black" />
+                        </TouchableOpacity>
+
+                        <ScrollView contentContainerStyle={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' }}>
                             {iconLibrary.map((icon, index) => (
-                                <TouchableOpacity key={index} onPress={() => setSelectedIcon(icon)} style={{ margin: 10 }}>
+                                <TouchableOpacity
+                                    key={index}
+                                    onPress={() => {
+                                        setSelectedIcon(icon);
+                                        setIconSelectionVisible(false);
+                                    }}
+                                    style={{ margin: 10 }}
+                                >
                                     <Image source={icon} style={{ width: 50, height: 50, borderRadius: 25 }} />
                                 </TouchableOpacity>
                             ))}
                         </ScrollView>
-                        <TouchableOpacity onPress={closeProfile} style={{ marginTop: 20, alignSelf: 'center' }}>
-                            <Text style={{ fontSize: 18, color: 'blue' }}>Annuler</Text>
-                        </TouchableOpacity>
                     </View>
                 </View>
             </Modal>
