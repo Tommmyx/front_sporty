f2ebe7bffe9c4928e6f87c3f11b668a2ec320f79
tomlecam <tom.lecam2001@gmail.com>
2024-12-05 19:00:39 +0100

    Updated React(or expo) version and added hanger icon

--- app/(tabs)/HomeScreen.tsx
+++ app/(tabs)/HomeScreen.tsx
@@ -1,12 +1,16 @@
 import React, { useRef, useEffect } from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity, StyleSheet  } from 'react-native';
 import { Canvas } from '@react-three/fiber';
 import { PerspectiveCamera, OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
 import Profile from './Profile';
+import BackgroundHomeScreen from '../../components/BackgroundHomeScreen';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faShirt } from '@fortawesome/free-solid-svg-icons';
 
 const AnimatedMesh = () => {
   const meshRef = useRef();
-  const { scene, animations } = useGLTF('../../assets/3D/avatar.glb'); 
+  const modelPath = require('../../assets/3D/avatar.glb');
+  const { scene, animations } = useGLTF(modelPath); 
   const { actions } = useAnimations(animations, scene); 
 
   useEffect(() => {
@@ -28,16 +32,44 @@ const AnimatedMesh = () => {
 };
 
 export default function HomeScreen({ navigation }) {
+  const handleButtonPress = () => {
+    console.log('Bouton de cintre pressé');
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f3f3f3' }}>
+    <View style={{ flex: 1 }}>
       <Profile navigation={navigation} />
-      <Canvas style={{ width: '100%', height: '100%' }}>
-        <PerspectiveCamera makeDefault position={[0, 0, 50]} fov={60} />
-        <ambientLight intensity={0.5} />
+      <BackgroundHomeScreen />
+      <Canvas style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+        <PerspectiveCamera makeDefault position={[0, 0, 100]} fov={80} />
+        <ambientLight intensity={0.9} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
         <AnimatedMesh />
         <OrbitControls />
       </Canvas>
+      <TouchableOpacity
+        style={styles.hangerButton}
+        onPress={handleButtonPress}
+      >
+        <FontAwesomeIcon icon={faShirt} /> 
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  hangerButton: {
+    position: 'absolute',
+    left: '35%', // Ajuste la position horizontale
+    top: '60%', // Centre verticalement
+    transform: [{ translateY: -15 }], // Centrage de l'icône (moitié de la taille)
+    backgroundColor: 'white',
+    borderRadius: 25,
+    padding: 10,
+    elevation: 5, // Ombre pour Android
+    shadowColor: '#000', // Ombre pour iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+});
