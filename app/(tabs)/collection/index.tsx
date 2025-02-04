6f6d4511de9cce09e7862b43cd71d72401b949c1
cedric_willaume <Willaume.cedric.sup@outlook.fr>
2025-02-04 23:22:37 +0100

    Ajout d'images

--- app/(tabs)/collection/index.tsx
+++ app/(tabs)/collection/index.tsx
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, TouchableOpacity, Switch, ScrollView, Image } f
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faTshirt, faBook, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'expo-router';
+import images from "../../../assets/images";
 
 const Collection = () => {
   const router = useRouter();
@@ -20,74 +21,74 @@ const Collection = () => {
   const allItems = {
     hauts: {
       tShirts: [
-        { name: "T-shirt manches courtes rouge", image: 'https://via.placeholder.com/100' },
-        { name: "T-shirt manches longues noir", image: 'https://via.placeholder.com/100' },
-        { name: "T-shirt oversize blanc", image: 'https://via.placeholder.com/100' },
-        { name: "Crop top rose", image: 'https://via.placeholder.com/100' },
+        { name: "T-shirt manches courtes rouge", image: images.tshirt1 },
+        { name: "T-shirt manches longues noir", image: images.tshirt2 },
+        { name: "T-shirt oversize blanc", image: images.tshirt3 },
+        { name: "Crop top rose", image: images.tshirt4 },
       ],
       chemises: [
-        { name: "Chemise unie blanche", image: 'https://via.placeholder.com/100' },
-        { name: "Chemise à carreaux bleue", image: 'https://via.placeholder.com/100' },
-        { name: "Chemise en jean", image: 'https://via.placeholder.com/100' },
-        { name: "Chemise hawaïenne", image: 'https://via.placeholder.com/100' },
+        { name: "Chemise unie blanche", image: images.chemise1 },
+        { name: "Chemise à carreaux bleue", image: images.chemise2 },
+        { name: "Chemise en jean", image: images.chemise3 },
+        { name: "Chemise hawaïenne", image: images.chemise4 },
       ],
       pulls: [
-        { name: "Pull col rond gris", image: 'https://via.placeholder.com/100' },
-        { name: "Pull col V noir", image: 'https://via.placeholder.com/100' },
-        { name: "Pull col roulé beige", image: 'https://via.placeholder.com/100' },
-        { name: "Pull à capuche bleu", image: 'https://via.placeholder.com/100' },
+        { name: "Pull col rond gris", image: images.pulls1 },
+        { name: "Pull col V noir", image: images.pulls2 },
+        { name: "Pull col roulé beige", image: images.pulls3 },
+        { name: "Pull à capuche bleu", image: images.pulls4 },
       ],
       vestes: [
-        { name: "Veste en cuir noire", image: 'https://via.placeholder.com/100' },
-        { name: "Veste en jean", image: 'https://via.placeholder.com/100' },
-        { name: "Bomber kaki", image: 'https://via.placeholder.com/100' },
-        { name: "Blazer gris", image: 'https://via.placeholder.com/100' },
+        { name: "Veste en cuir noire", image: images.veste1 },
+        { name: "Veste en jean", image: images.veste2 },
+        { name: "Bomber kaki", image: images.veste3 },
+        { name: "Blazer gris", image: images.veste4 },
       ],
     },
     bas: {
       jeans: [
-        { name: "Jean slim noir", image: 'https://via.placeholder.com/100' },
-        { name: "Jean droit bleu", image: 'https://via.placeholder.com/100' },
-        { name: "Jean déchiré", image: 'https://via.placeholder.com/100' },
-        { name: "Jean bootcut", image: 'https://via.placeholder.com/100' },
+        { name: "Jean slim noir", image: images.jeans1 },
+        { name: "Jean droit bleu", image: images.jeans2 },
+        { name: "Jean déchiré", image: images.jeans3 },
+        { name: "Jean bootcut", image: images.jeans4 },
       ],
       pantalons: [
-        { name: "Pantalon chino beige", image: 'https://via.placeholder.com/100' },
-        { name: "Pantalon cargo vert", image: 'https://via.placeholder.com/100' },
-        { name: "Pantalon de survêtement gris", image: 'https://via.placeholder.com/100' },
-        { name: "Pantalon tailleur noir", image: 'https://via.placeholder.com/100' },
+        { name: "Pantalon chino beige", image: images.pantalon1 },
+        { name: "Pantalon cargo vert", image: images.pantalon2 },
+        { name: "Pantalon de survêtement gris", image: images.pantalon3 },
+        { name: "Pantalon tailleur noir", image: images.pantalon4 },
       ],
       shorts: [
-        { name: "Short en jean bleu", image: 'https://via.placeholder.com/100' },
-        { name: "Short de sport noir", image: 'https://via.placeholder.com/100' },
-        { name: "Short cargo beige", image: 'https://via.placeholder.com/100' },
-        { name: "Short en lin blanc", image: 'https://via.placeholder.com/100' },
+        { name: "Short en jean bleu", image: images.short1 },
+        { name: "Short de sport noir", image: images.short2 },
+        { name: "Short cargo beige", image: images.short3 },
+        { name: "Short en lin blanc", image: images.short4 },
       ],
       jupes: [
-        { name: "Jupe courte noire", image: 'https://via.placeholder.com/100' },
-        { name: "Jupe midi rouge", image: 'https://via.placeholder.com/100' },
-        { name: "Jupe longue verte", image: 'https://via.placeholder.com/100' },
-        { name: "Jupe plissée beige", image: 'https://via.placeholder.com/100' },
+        { name: "Jupe courte noire", image: images.jupe1 },
+        { name: "Jupe midi rouge", image: images.jupe2 },
+        { name: "Jupe longue verte", image: images.jupe3 },
+        { name: "Jupe plissée beige", image: images.jupe4 },
       ],
     },
     chaussures: {
       sneakers: [
-        { name: "Sneakers basses blanches", image: 'https://via.placeholder.com/100' },
-        { name: "Sneakers montantes noires", image: 'https://via.placeholder.com/100' },
-        { name: "Sneakers rétro rouges", image: 'https://via.placeholder.com/100' },
-        { name: "Sneakers running bleues", image: 'https://via.placeholder.com/100' },
+        { name: "Sneakers basses blanches", image: images.sneakers1 },
+        { name: "Sneakers montantes noires", image: images.sneakers2 },
+        { name: "Sneakers rétro rouges", image: images.sneakers3 },
+        { name: "Sneakers running bleues", image: images.sneakers4 },
       ],
       bottes: [
-        { name: "Bottes classiques marron", image: 'https://via.placeholder.com/100' },
-        { name: "Bottes motardes noires", image: 'https://via.placeholder.com/100' },
-        { name: "UGG beiges", image: 'https://via.placeholder.com/100' },
-        { name: "Bottines grises", image: 'https://via.placeholder.com/100' },
+        { name: "Bottes classiques marron", image: images.bottes1 },
+        { name: "Bottes motardes noires", image: images.bottes2 },
+        { name: "UGG beiges", image: images.bottes3 },
+        { name: "Bottines grises", image: images.bottes4 },
       ],
     },
     accessoires: {
       chapeaux: [
-        { name: "Casquette rouge", image: 'https://via.placeholder.com/100' },
-        { name: "Bob jaune", image: 'https://via.placeholder.com/100' },
+        { name: "Casquette rouge", image: images.chapeau1 },
+        { name: "Bob jaune", image: images.chapeau2 },
       ],
     },
   };
@@ -194,7 +195,7 @@ const Collection = () => {
                             ]}
                           >
                             <Image
-                              source={{ uri: item.image }}
+                              source={item.image}
                               style={[
                                 styles.itemImage,
                                 isSwitchOn && isOwned && styles.ownedItemImage,
--- assets/images/index.js
+++ assets/images/index.js
@@ -1,4 +1,59 @@
 export default {
-    chemise: require("./clothing/haut/chemise/1.png")
+    // Haut
+    chemise1: require("./clothing/haut/chemise/1.png"),
+    chemise2: require("./clothing/haut/chemise/2.png"),
+    chemise3: require("./clothing/haut/chemise/3.png"),
+    chemise4: require("./clothing/haut/chemise/4.png"),
+
+    pulls1: require("./clothing/haut/pulls/1.png"),
+    pulls2: require("./clothing/haut/pulls/2.png"),
+    pulls3: require("./clothing/haut/pulls/3.png"),
+    pulls4: require("./clothing/haut/pulls/4.png"),
+
+    tshirt1: require("./clothing/haut/tshirt/1.png"),
+    tshirt2: require("./clothing/haut/tshirt/2.png"),
+    tshirt3: require("./clothing/haut/tshirt/3.png"),
+    tshirt4: require("./clothing/haut/tshirt/4.png"),
+
+    veste1: require("./clothing/haut/veste/1.png"),
+    veste2: require("./clothing/haut/veste/2.png"),
+    veste3: require("./clothing/haut/veste/3.png"),
+    veste4: require("./clothing/haut/veste/4.png"),
+
+    // Bas
+    jeans1: require("./clothing/bas/jeans/1.png"),
+    jeans2: require("./clothing/bas/jeans/2.png"),
+    jeans3: require("./clothing/bas/jeans/3.png"),
+    jeans4: require("./clothing/bas/jeans/4.png"),
+
+    jupe1: require("./clothing/bas/jupe/1.png"),
+    jupe2: require("./clothing/bas/jupe/2.png"),
+    jupe3: require("./clothing/bas/jupe/3.png"),
+    jupe4: require("./clothing/bas/jupe/4.png"),
+
+    pantalon1: require("./clothing/bas/pantalon/1.png"),
+    pantalon2: require("./clothing/bas/pantalon/2.png"),
+    pantalon3: require("./clothing/bas/pantalon/3.png"),
+    pantalon4: require("./clothing/bas/pantalon/4.png"),
+
+    short1: require("./clothing/bas/short/1.png"),
+    short2: require("./clothing/bas/short/2.png"),
+    short3: require("./clothing/bas/short/3.png"),
+    short4: require("./clothing/bas/short/4.png"),
+
+    // Chaussures
+    bottes1: require("./clothing/chaussure/bottes/1.png"),
+    bottes2: require("./clothing/chaussure/bottes/2.png"),
+    bottes3: require("./clothing/chaussure/bottes/3.png"),
+    bottes4: require("./clothing/chaussure/bottes/4.png"),
+
+    sneakers1: require("./clothing/chaussure/sneakers/1.png"),
+    sneakers2: require("./clothing/chaussure/sneakers/2.png"),
+    sneakers3: require("./clothing/chaussure/sneakers/3.png"),
+    sneakers4: require("./clothing/chaussure/sneakers/4.png"),
+
+    // Accessoires
+    chapeau1: require("./clothing/accessoire/chapeau/1.png"),
+    chapeau2: require("./clothing/accessoire/chapeau/2.png"),
   };
   
